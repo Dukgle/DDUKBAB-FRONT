37337c6b58f99e2003507d59c40d551793027661
pookey1104 <pookey1104@naver.com>
2023-10-04 05:40:17 +0900

    [FEAT] Splash, SignUp 페이지 구현 #pookey1104

--- src/App.js
+++ src/App.js
@@ -1,40 +1,42 @@
-import './App.css';
+import "./App.css";
 import React, { useState } from "react";
 import { BrowserRouter, Route, Routes, Switch, Link, RouterProvider } from "react-router-dom";
 
-import Splash from './verification/Splash';
-import Login from './verification/Login';
-import SignUp from './verification/SignUp';
+import Splash from "./verification/Splash";
+import Login from "./verification/Login";
+import SignUp from "./verification/SignUp";
 
-import MyPage from './inform/MyPage';
-import Information from './inform/Information';
-import Reservation from './reservation/Reservation';
+import MyPage from "./inform/MyPage";
+import Information from "./inform/Information";
+import Reservation from "./reservation/Reservation";
 import ReviewMain from "./review/ReviewMain";
 
-import Tournament from './tournament/Tournament';
+import Tournament from "./tournament/Tournament";
 
-import MenuMain from './menu/MenuMain';
-import Cart from './menu/Cart';
-import CartBookmark from './menu/CartBookmark';
+import MenuMain from "./menu/MenuMain";
+import Cart from "./menu/Cart";
+import CartBookmark from "./menu/CartBookmark";
 
 function App() {
   return (
     <>
-      <Routes>
-        <Route path='/' element={<Splash />} />             {/* 첫 번째 페이지 */} 
-        <Route path='/login' element={<Login />} />
-        <Route path='/review' element={<ReviewMain />} />   {/* 후기게시판 메인 페이지 */}
-        <Route path='/signup' element={<SignUp />} />       {/* 회원가입 */}
-        <Route path='/mypage' element={<MyPage />} />
-        <Route path='/information' element={<Information />} />   {/* 내 정보 */}
-        <Route path='/menu' element={<MenuMain />} />             {/* 메뉴 메인 페이지 */}
-        <Route path='/reservation' element={<Reservation />} />   {/* 자리 예약 */}
-        <Route path='/cart' element={<Cart />} />
-        <Route path='/tournament' element={<Tournament />} />
-        <Route path='/bookmark' element={<CartBookmark />} />
-      </Routes>
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Splash />} /> {/* 첫 번째 페이지 */}
+          <Route path="/login" element={<Login />} />
+          <Route path="/review" element={<ReviewMain />} /> {/* 후기게시판 메인 페이지 */}
+          <Route path="/signup" element={<SignUp />} /> {/* 회원가입 */}
+          <Route path="/mypage" element={<MyPage />} />
+          <Route path="/information" element={<Information />} /> {/* 내 정보 */}
+          <Route path="/menu" element={<MenuMain />} /> {/* 메뉴 메인 페이지 */}
+          <Route path="/reservation" element={<Reservation />} /> {/* 자리 예약 */}
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/tournament" element={<Tournament />} />
+          <Route path="/bookmark" element={<CartBookmark />} />
+        </Routes>
+      </div>
     </>
-);
+  );
 }
 
 export default App;
--- src/verification/SignUp.jsx
+++ src/verification/SignUp.jsx
@@ -1,15 +1,87 @@
-import React from 'react';
-import Header from '../header/Header_verify';
+import React from "react";
+import Header from "../header/Header_verify";
+import "./SignUp.css";
+import { Link } from "react-router-dom";
+
+import down from "../icon/arrow-down.png";
+import none from "../icon/circle-x.png";
 
 function SignUp() {
-    const logoText = "회원가입";
+  const logoText = "회원가입";
 
-    return (
-        <div className="signUp-page">
-            <Header logoText={logoText} />
-
-        </div>
-    );
+  return (
+    <div className="signUp-page">
+      <Header logoText={logoText} />
+      <div className="contents">
+        <form action="" className="form" method="post">
+          <div className="form-box">
+            <div className="form-box-input">
+              <p>역할</p>
+              <select name="major" id="input-dropdown">
+                <option disabled selected value="">
+                  역할을 선택해주세요.
+                </option>
+                <option value="">학생</option>
+                <option value="">교직원</option>
+                <option value="">판매자</option>
+              </select>
+            </div>
+            <div className="form-box-check">
+              <img src={down} alt="down" />
+            </div>
+          </div>
+          <div className="form-box">
+            <div className="form-box-input">
+              <p>이름</p>
+              <input type="text" name="" id="name" className="input-field" placeholder="이름 입력" required />
+            </div>
+            <div className="form-box-check">
+              <img src={none} alt="none" />
+            </div>
+          </div>
+          <div className="form-box">
+            <div className="form-box-input">
+              <p>닉네임</p>
+              <input type="text" name="" id="username" className="input-field" placeholder="닉네임 입력" required />
+            </div>
+            <div className="form-box-check">
+              <img src={none} alt="none" />
+            </div>
+          </div>
+          <div className="form-box">
+            <div className="form-box-input">
+              <p>학번/사번</p>
+              <input type="text" name="" id="number" className="input-field" placeholder="학번/사번 입력" required />
+            </div>
+            <div className="form-box-check">
+              <img src={none} alt="none" />
+            </div>
+          </div>
+          <div className="form-box">
+            <div className="form-box-input">
+              <p>비밀번호</p>
+              <input type="password" name="" id="password" className="input-field" placeholder="비밀번호 입력" required />
+            </div>
+            <div className="form-box-check">
+              <img src={none} alt="none" />
+            </div>
+          </div>
+          <div className="form-box">
+            <div className="form-box-input">
+              <p>비밀번호 확인</p>
+              <input type="password" name="" id="password2" className="input-field" placeholder="비밀번호 입력" required />
+            </div>
+            <div className="form-box-check">
+              <img src={none} alt="none" />
+            </div>
+          </div>
+          <Link to="/">
+            <input id="signup" type="submit" className="submit-btn" value="회원가입 완료하기" />
+          </Link>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default SignUp;
