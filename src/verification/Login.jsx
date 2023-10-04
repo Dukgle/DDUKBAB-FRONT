2a962b0c94d9ee393e44e297ea822f9353dd11f7
pookey1104 <pookey1104@naver.com>
2023-10-05 06:23:55 +0900

    [FIX] SignUp, Login js 반영 #pookey1104

--- src/verification/Login.jsx
+++ src/verification/Login.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../header/Header_verify";
 import { Link } from "react-router-dom";
 import "./Login.css";
@@ -9,20 +9,41 @@ import Apple from "../icon/apple.png";
 function Login() {
   const logoText = "로그인";
 
+  const [number, setNumber] = useState("");
+  const [password, setPassword] = useState("");
+
+  const validatePassword = () => {
+    // 비밀번호 조건을 만족하는지 확인
+    const regex = /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    return regex.test(password);
+  };
+
+  const isFormValid = number !== "" && password !== "" && validatePassword();
+
   return (
     <div className="login-page">
       <Header logoText={logoText} />
       <div className="things">
         <img src={Logo} alt="Logo" />
-        <form action="" className="form" method="post">
+        <form action="" className="form2" method="post">
           <div className="form-box-input-lg">
-            <input type="text" name="" id="number" className="input-field-lg" placeholder="학번/사번/가게명" required />
+            <input type="text" name="" id="number" className="input-field-lg" placeholder="학번/사번/가게명" value={number} onChange={(e) => setNumber(e.target.value)} required />
           </div>
           <div className="form-box-input-lg">
-            <input type="password" name="" id="password" className="input-field-lg" placeholder="비밀번호" required />
+            <input type="password" name="" id="password" className="input-field-lg" placeholder="비밀번호" value={password} onChange={(e) => setPassword(e.target.value)} required />
           </div>
           <Link to="/menu">
-            <input id="signup" type="submit" className="login-btn" value="로그인" />
+            <input
+              id="signup"
+              type="submit"
+              className="login-btn"
+              value="로그인"
+              style={{
+                backgroundColor: isFormValid ? "#fccb6f" : "white",
+                cursor: isFormValid ? "pointer" : "not-allowed",
+              }}
+              disabled={!isFormValid}
+            />
           </Link>
         </form>
         <div className="apple-login">
