2909f6c2c929382fd95503239ae7252adfa9a00d
pookey1104 <pookey1104@naver.com>
2023-10-06 04:22:06 +0900

    [FIX] Header 수정 #pookey1104

--- src/header/Header_verify.jsx
+++ src/header/Header_verify.jsx
@@ -1,16 +1,14 @@
 // login 페이지와 signup 페이지에서 사용하는 헤더_아이콘과 닉네임 없음
 
-import './Header_verify.css';
-import React, {useState} from 'react';
-
+import "./Header_verify.css";
+import React, { useState } from "react";
 
 function Header({ logoText }) {
-    return (
-        <header className='verify'>
-            <div className="logo">{logoText}</div>
-            
-        </header>
-    );
+  return (
+    <header className="verify">
+      <div className="logo">{logoText}</div>
+    </header>
+  );
 }
 
 export default Header;
