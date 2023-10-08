5025b91523a567b81414ca1de1f363d35d5449f2
pookey1104 <pookey1104@naver.com>
2023-10-08 14:33:09 +0900

    [FIX] Header_option 수정 #pookey1104

--- src/header/Header_option.jsx
+++ src/header/Header_option.jsx
@@ -1,57 +1,61 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
-import CartIcon from '../icon/icon-shopping-cart.png';
-import ListIcon from '../icon/icon-ui.png';
+import CartIcon from "../icon/icon-shopping-cart.png";
+import ListIcon from "../icon/icon-ui.png";
 
-import Cart from '../cart/Cart';
-import Qr from './Qr';
-import Dropdown from './Dropdown';
+import Cart from "../cart/Cart";
+import Qr from "./Qr";
+import Dropdown from "./Dropdown";
 
-function Header_option({ logoText}) {
-    const [isDropdownView, setDropdownView] = useState(false);
+function Header_option({ logoText }) {
+  const [isDropdownView, setDropdownView] = useState(false);
 
-    const handleClickContainer = () => {
-        setDropdownView(!isDropdownView);
-    };
+  const handleClickContainer = () => {
+    setDropdownView(!isDropdownView);
+  };
 
-    const handleBlurContainer = () => {
-        setTimeout(() => {
-            setDropdownView(false);
-        }, 200);
-    };
+  const handleBlurContainer = () => {
+    setTimeout(() => {
+      setDropdownView(false);
+    }, 200);
+  };
 
-    return (
-        <header>
-            <div className='back'>
-                <Link to={`/${logoText}`}>
-                    <button className='back-button'>{logoText}&gt;</button>
+  return (
+    <header>
+      <div className="header-menu">
+        <div className="back">
+          <Link to={`/${logoText}`}>
+            <button className="back-button">{logoText}&gt;</button>
+          </Link>
+        </div>
+        <div className="nickname" id="menu_nickname">
+          닉네임
+        </div>
+        <div className="logo">{logoText}</div>
+        <div className="icon-wrap">
+          <div className="icon">
+            <div className="cart">
+              <div className="cart-button">
+                <Link to="/cart">
+                  <img src={CartIcon} alt="Cart" />
                 </Link>
+              </div>
             </div>
-            <div className='nickname' id='menu_nickname'>닉네임</div>
-            <div className="logo">{logoText}</div>
-            <div className='icon-wrap'>
-                <div className='icon'>
-                    <div className='cart'>
-                        <div className='cart-button'>
-                            <Link to="/cart">
-                                <img src={CartIcon} alt="Cart" />
-                            </Link>
-                        </div>
-                    </div>
-                    <div className='qr'>
-                        <Qr />
-                    </div>
-                    <div className='list' onBlur={handleBlurContainer}>
-                        <button className="list-button" onClick={handleClickContainer}>
-                            <img src={ListIcon} alt="List" />
-                            {isDropdownView && <Dropdown />}
-                        </button>
-                    </div>
-                </div>
+            <div className="qr">
+              <Qr />
             </div>
-        </header>
-    );
+            <div className="list" onBlur={handleBlurContainer}>
+              <button className="list-button" onClick={handleClickContainer}>
+                <img src={ListIcon} alt="List" />
+                {isDropdownView && <Dropdown />}
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 }
 
 export default Header_option;
